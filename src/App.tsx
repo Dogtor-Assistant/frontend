f31eeb3c9a5188cd9f53a70d577669f3006d219f
Mathias Quintero <me@quintero.io>
2021-05-27 00:54:56 +0200

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+
+import Home from 'Home';
+import Login from 'Login';
 
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <Router>
+            <div>
+                <h1>Dogtor</h1>
+
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
