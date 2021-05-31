7cf0420684912ec425eb404fb16a290418593c47
Mathias Quintero <me@quintero.io>
2021-05-31 15:59:37 +0200

    Including authentication provider at the base of the App

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from 'App';
 
+import { AuthenticationProvider } from 'authentication';
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <AuthenticationProvider>
+            <App />
+        </AuthenticationProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
