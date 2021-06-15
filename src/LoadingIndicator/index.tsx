86f6d9c770b8186991d4bee0e269bc1fcd172fe6
Mathias Quintero <me@quintero.io>
2021-06-15 19:17:40 +0200

    Adding custom loading indicator that's a dog walking

---
+++ src/LoadingIndicator/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import { useColorModeValue } from '@chakra-ui/react';
+
+import dark from './dark.json';
+import light from './light.json';
+
+type Props = {
+    size?: number
+}
+
+function LoadingIndicator(props: Props) {
+    const size = props.size ?? 120;
+    const animationData = useColorModeValue(light, dark);
+    return (
+        <Lottie
+            height={size}
+            options={{
+                animationData,
+            }}
+            width={size}
+        />
+    );
+}
+
+export default LoadingIndicator;
