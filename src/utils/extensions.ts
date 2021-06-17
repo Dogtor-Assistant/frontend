d6f55210d83b5554d5598029da7cfb3162c39f25
Mathias Quintero <me@quintero.io>
2021-06-17 21:41:05 +0200

    Adding snippet for pagination fragment components

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -1,6 +1,14 @@
 import type { Rect } from './types';
 
 declare global {
+    interface Array<T> {
+        compactMap<O>(transform: (element: T) => O | null | undefined): Array<O>;
+    }
+
+    interface ReadonlyArray<T> {
+        compactMap<O>(transform: (element: T) => O | null | undefined): Array<O>;
+    }
+
     interface HTMLElement {
         getRect(): Rect
     }
@@ -14,6 +22,10 @@ HTMLElement.prototype.getRect = function<T extends HTMLElement>(this: T): Rect {
         parent: this.parentElement?.getRect() ?? null,
         width: this.clientWidth,
     };
+};
+
+Array.prototype.compactMap = function<T, O>(this: T[], transform: (element: T) => O | null | undefined): O[] {
+    return this.map(transform).filter((x): x is O => x != null);
 };
 
 export {};
