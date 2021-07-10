14f516d16473a1110319fb6d4f21cc546e58e594
Pellumb Baboci <pellumbbaboci@gmail.com>
2021-07-11 01:11:15 +0200

    adding toast and changing event object

--- src/Scheduler/AppointmentDone.tsx
+++ src/Scheduler/AppointmentDone.tsx
@@ -1,3 +1,4 @@
+import type { ActivityLevel, Gender } from '../Signup/__generated__/PatientUserCreateMutation.graphql';
 import type { AppointmentDoneMutation } from './__generated__/AppointmentDoneMutation.graphql';
 
 import React from 'react';
@@ -9,32 +10,55 @@ import { useMutation } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
 type AppointmentType = {
-    event: {
+    
+    event:{
         id:string,
         title: string,
         start: string | number | Date | number[],
         end: string | number | Date | number[],
         lastName: string,
+        firstName: string,
+        patient: {
+            activityLevel: ActivityLevel,
+            address: {
+                city: string,
+                streetName: string,
+                streetNumber: number,
+                zipCode: number,
+            },
+            firstname: string,
+            gender: Gender,
+            height: number,
+            id: string,
+            isSmoker: boolean,
+            lastname: string,
+            surgeries: [string],
+            weight: number,
+        },
+
     },
-    onClose: ()=>void,
+    onClose: ()=>void, //UseDisclosureProps['onClose'],
 }
 
 const AppointmentDone = ({ event, onClose }:AppointmentType) => {
+    
     const toast = useToast();
 
     const [commit] = useMutation<AppointmentDoneMutation>(graphql`
     mutation AppointmentDoneMutation($id: ID!){
         makeAppointmentAsDone(id: $id)
-        }
+    }
     `);
 
     const handleDoneClick = (): void => {
 
         const result = confirm('Are you sure you want to mark this appointment as done?');
+        
         if (result) {
             commit({
                 onCompleted({ makeAppointmentAsDone }) {
                     if(makeAppointmentAsDone && onClose) {
+                        
                         toast({
                             description: 'Appointment marked as Done and been saved.',
                             duration: 9000,
@@ -43,6 +67,7 @@ const AppointmentDone = ({ event, onClose }:AppointmentType) => {
                             title: 'Appointment marked as Done.',
                         });
                         onClose();
+                        
                     }else{
                         toast({
                             description: 'Appointment could not be marked as Done.',
@@ -66,6 +91,7 @@ const AppointmentDone = ({ event, onClose }:AppointmentType) => {
             mr={3}
             onClick={handleDoneClick}
         >
+        
         Done
         </Button>
     );
@@ -73,4 +99,3 @@ const AppointmentDone = ({ event, onClose }:AppointmentType) => {
 };
 
 export default AppointmentDone;
-
