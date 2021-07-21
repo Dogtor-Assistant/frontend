641ca29f70963e8be1b4c0b50979fc7e5fdd93d5
Mathias Quintero <me@quintero.io>
2021-07-21 19:18:52 +0200

    Preventing refreshes when translating the search parameters

--- src/Search/Search.tsx
+++ src/Search/Search.tsx
@@ -1,12 +1,29 @@
-import React, { useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
+
+import React, { useMemo, useRef } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import SearchFromId from './SearchFromId';
 import SearchRoot from './SearchRoot';
 
 function Search() {
+    const history = useHistory();
     const { search } = useLocation();
-    const params = useMemo(() => new URLSearchParams(search), [search]);
+    const previousHistoryLength = useRef<number | null>();
+    const previousParams = useRef<URLSearchParams | null>(null);
+    const params = useMemo(() => {
+        if (
+            previousParams.current != null &&
+            previousHistoryLength.current != null &&
+            previousHistoryLength.current === history.length
+        ) {
+            return previousParams.current;
+        }
+
+        const params = new URLSearchParams(search);
+        previousParams.current = params;
+        previousHistoryLength.current = history.length;
+        return params;
+    }, [history.length, search]);
 
     const searchId = params.get('id');
     if (searchId != null) {
