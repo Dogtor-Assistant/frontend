81b4797e398736a6060720069ca782e5bbadbdf6
konpap99 <k.papaioannou@tum.de>
2021-07-22 17:06:00 +0200

    Fix relay problem

--- src/PatientPage/__generated__/CheckupNotUnreadQuery.graphql.ts
+++ src/PatientPage/__generated__/CheckupNotUnreadQuery.graphql.ts
@@ -3,24 +3,18 @@
 // @ts-nocheck
 
 import { ConcreteRequest } from "relay-runtime";
-export type Insurance = "Private" | "Public" | "%future added value";
 export type CheckupNotUnreadQueryVariables = {};
 export type CheckupNotUnreadQueryResponse = {
     readonly me: {
         readonly firstname: string;
         readonly lastname: string;
         readonly patientProfile: {
-            readonly insurance: Insurance;
             readonly address: {
-                readonly streetName: string;
-                readonly streetNumber: number;
                 readonly city: string;
-                readonly zipCode: number;
             };
             readonly unreadCheckups: ReadonlyArray<{
                 readonly id: string;
                 readonly services: ReadonlyArray<string>;
-                readonly suggestedDate: string;
             }>;
         } | null;
     } | null;
@@ -38,17 +32,12 @@ query CheckupNotUnreadQuery {
     firstname
     lastname
     patientProfile {
-      insurance
       address {
-        streetName
-        streetNumber
         city
-        zipCode
       }
       unreadCheckups {
         id
         services
-        suggestedDate
       }
       id
     }
@@ -75,13 +64,6 @@ v1 = {
 v2 = {
   "alias": null,
   "args": null,
-  "kind": "ScalarField",
-  "name": "insurance",
-  "storageKey": null
-},
-v3 = {
-  "alias": null,
-  "args": null,
   "concreteType": "Address",
   "kind": "LinkedField",
   "name": "address",
@@ -91,41 +73,20 @@ v3 = {
       "alias": null,
       "args": null,
       "kind": "ScalarField",
-      "name": "streetName",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "streetNumber",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
       "name": "city",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "zipCode",
       "storageKey": null
     }
   ],
   "storageKey": null
 },
-v4 = {
+v3 = {
   "alias": null,
   "args": null,
   "kind": "ScalarField",
   "name": "id",
   "storageKey": null
 },
-v5 = {
+v4 = {
   "alias": null,
   "args": null,
   "concreteType": "Checkup",
@@ -133,19 +94,12 @@ v5 = {
   "name": "unreadCheckups",
   "plural": true,
   "selections": [
-    (v4/*: any*/),
+    (v3/*: any*/),
     {
       "alias": null,
       "args": null,
       "kind": "ScalarField",
       "name": "services",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "suggestedDate",
       "storageKey": null
     }
   ],
@@ -177,8 +131,7 @@ return {
             "plural": false,
             "selections": [
               (v2/*: any*/),
-              (v3/*: any*/),
-              (v5/*: any*/)
+              (v4/*: any*/)
             ],
             "storageKey": null
           }
@@ -214,27 +167,26 @@ return {
             "plural": false,
             "selections": [
               (v2/*: any*/),
-              (v3/*: any*/),
-              (v5/*: any*/),
-              (v4/*: any*/)
+              (v4/*: any*/),
+              (v3/*: any*/)
             ],
             "storageKey": null
           },
-          (v4/*: any*/)
+          (v3/*: any*/)
         ],
         "storageKey": null
       }
     ]
   },
   "params": {
-    "cacheID": "99f5960a8d1ae76596ea97efda198cd5",
+    "cacheID": "8e2846abb4a7b73f3ec27ced9e1ac77d",
     "id": null,
     "metadata": {},
     "name": "CheckupNotUnreadQuery",
     "operationKind": "query",
-    "text": "query CheckupNotUnreadQuery {\n  me {\n    firstname\n    lastname\n    patientProfile {\n      insurance\n      address {\n        streetName\n        streetNumber\n        city\n        zipCode\n      }\n      unreadCheckups {\n        id\n        services\n        suggestedDate\n      }\n      id\n    }\n    id\n  }\n}\n"
+    "text": "query CheckupNotUnreadQuery {\n  me {\n    firstname\n    lastname\n    patientProfile {\n      address {\n        city\n      }\n      unreadCheckups {\n        id\n        services\n      }\n      id\n    }\n    id\n  }\n}\n"
   }
 };
 })();
-(node as any).hash = '73a5cdf91d53cf08066a26d04baa8470';
+(node as any).hash = 'c45c0172c09643877c1a2fbae30f59aa';
 export default node;
