b1c5d6307796d6dd6ae68f40fc640d469ae4506f
Mathias Quintero <me@quintero.io>
2021-07-22 15:28:11 +0200

    Adding dark theme to map

--- src/Search/Results/DoctorDetails/DoctorLocationCard.tsx
+++ src/Search/Results/DoctorDetails/DoctorLocationCard.tsx
@@ -5,6 +5,7 @@ import {
     Badge,
     Box,
     Text,
+    useColorModeValue,
     VStack,
 } from '@chakra-ui/react';
 import Map from 'google-map-react';
@@ -12,7 +13,13 @@ import Map from 'google-map-react';
 import { useFragment } from 'react-relay';
 import { graphql } from 'babel-plugin-relay/macro';
 
+import googleMapsDarkTheme from './googleMapsDarkTheme';
+
 import { GOOGLE_MAPS_API_KEY } from 'utils/constants';
+
+const darkModeProps: Partial<Map.Props['options']> = {
+    styles: googleMapsDarkTheme,
+};
 
 type Props = {
     doctor: DoctorLocationCard_doctor$key,
@@ -43,6 +50,8 @@ function DoctorLocationCard(props: Props) {
         };
     }
 
+    const style = useColorModeValue({}, darkModeProps);
+
     return (
         <VStack align="start">
             <Text
@@ -64,6 +73,9 @@ function DoctorLocationCard(props: Props) {
                         lat: doctor.address.coordinates.latitude,
                         lng: doctor.address.coordinates.longitude,
                     }}
+                    options={{
+                        ...style,
+                    }}
                     zoom={17}
                 >
                     { /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */ }
@@ -71,7 +83,9 @@ function DoctorLocationCard(props: Props) {
                     <div lat={doctor.address.coordinates.latitude} lng={doctor.address.coordinates.longitude}>
                         <Badge
                             borderRadius="xl"
+                            colorScheme="teal"
                             px="2"
+                            variant="solid"
                         >
                             Dr. {doctor.lastname}
                         </Badge>
