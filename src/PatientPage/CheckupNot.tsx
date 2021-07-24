491e74a435295c8c4c9c0613561cf428b7bb1033
Mathias Quintero <me@quintero.io>
2021-07-24 13:45:11 +0200

    Simplifying checkups

--- src/PatientPage/CheckupNot.tsx
+++ src/PatientPage/CheckupNot.tsx
@@ -4,12 +4,7 @@ import type { PreloadedQuery } from 'react-relay';
 
 import CheckupNotUnreadQuery from './__generated__/CheckupNotUnreadQuery.graphql';
 
-import React, {
-    useCallback,
-    useEffect,
-    useRef,
-    useState,
-} from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Center, Grid } from '@chakra-ui/react';
 
 import { useQueryLoader } from 'react-relay';
@@ -18,10 +13,6 @@ import { graphql } from 'babel-plugin-relay/macro';
 
 import Suspense from 'Suspense';
 import NotBox from './NotBox';
-
-import { useAuthenticatedToken } from 'authentication';
-import { useBackendURL } from 'config';
-import { usePatientId } from 'user';
 
 type LoadedProps = {
   data: PreloadedQuery<CheckupNotUnreadQueryType>,
@@ -73,80 +64,6 @@ function LoadedCheckupNot(props: LoadedProps) {
 }
 
 function CheckupNot() {
-    const url = useBackendURL('graphql');
-    const accessToken = useAuthenticatedToken();
-    const patientId = usePatientId();
-
-    const [refreshCheckups, setRefreshCheckups] = useState(true);
-    
-    const fetchRec = useCallback(
-        async () => {
-            const token = await accessToken();
-            const authorizationHeader = token != null ? {
-                authorization: `Bearer ${token}`,
-            } : undefined;
-
-            const response = await fetch(url, {
-                body: JSON.stringify({
-                    query: `query CheckupNotRecommendationsQuery {
-                                me {
-                                    patientProfile {
-                                        checkupRecommendations {
-                                            service
-                                            kind
-                                            periodInDays
-                                        }
-                                    }
-                                }
-                            }`,
-                }),
-                headers: {
-                    'accept': 'application/json',
-                    'content-type': 'application/json',
-                    ...authorizationHeader,
-                },
-                method: 'POST',
-                mode: 'cors',
-            });
-            return response.json();
-        },
-        [url, accessToken],
-    );
-    
-    const genCheck = useCallback(
-        async rec => {
-            const token = await accessToken();
-            const authorizationHeader = token != null ? {
-                authorization: `Bearer ${token}`,
-            } : undefined;
-
-            const variables = { 'id': patientId, 'recommendations': rec };
-            const response = await fetch(url, {
-                body: JSON.stringify({
-                    query: `mutation generatePatientCheckup($input: CheckupsGenInput!){
-                                generateCheckups(input: $input) {
-                                    services 
-                                    suggestedDate
-                                    isRead
-                                }
-                            }`,
-                    variables: {
-                        input: variables,
-                    },
-                }),
-                headers: {
-                    'accept': 'application/json',
-                    'content-type': 'application/json',
-                    ...authorizationHeader,
-                },
-                method: 'POST',
-                mode: 'cors',
-            });
-            return response.json();
-        },
-        [accessToken, patientId, url],
-    );
-
     const [
         data,
         loadQuery,
@@ -155,25 +72,12 @@ function CheckupNot() {
 
     const error = useRef<ErrorBoundary>(null);
     useEffect(() => {
-        if (refreshCheckups) {
-            let rec: Array<{ 'service': string, 'kind': string, 'periodInDays': number }> = [];
-            setRefreshCheckups(false);
-            fetchRec().then(res => {
-                rec = res.data.me.patientProfile.checkupRecommendations;
-            }).then(() => {
-                genCheck(rec).then(() => {
-                    loadQuery({ }, { fetchPolicy: 'network-only' });
-                });
-            });
-            
-        }
-
         error.current?.reset();
         loadQuery({ });
         return () => {
             dispose();
         };
-    }, [dispose, fetchRec, genCheck, loadQuery, refreshCheckups, url]);
+    }, [dispose, loadQuery]);
 
     return (
         <Suspense boundaryRef={error}>
